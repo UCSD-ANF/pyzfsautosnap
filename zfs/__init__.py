6247f208843d4249224da835466701fc503086e9
Geoff Davis <gadavis@ucsd.edu>
2014-11-20 14:33:29 -0800

    Update exception types
    Add a couple of new ones for filesystem structures. Changed the
    inheritence hierarchy to work better with python default exceptions.
    Added docstrings to all classes.

--- zfs/__init__.py
+++ zfs/__init__.py
@@ -1,12 +1,39 @@
-class ZfsError(Exception):
+class ZfsOSError(OSError):
+    """Base OS Level Exception for ZFS problems
+
+    Used for command execution issues with underlying ZFS commands, including
+    entities existing or not existing when expected.
+    """
     pass
 
-class ZfsNoDatasetError(ZfsError):
+class ZfsNoDatasetError(ZfsOSError):
+    """The dataset, filesystem, or snapshot does not exist"""
     pass
 
-class ZfsNoPoolError(ZfsError): pass
-
-class ZfsPermissionError(ZfsError):
+class ZfsDatasetExistsError(ZfsOSError):
+    """The dataset, filesystem, or snapshot already exists"""
     pass
 
-class ZfsBadFsName(ZfsError): pass
+class ZfsNoPoolError(ZfsOSError):
+    """The zpool does not exist"""
+    pass
+
+class ZfsPoolExistsError(ZfsOSError):
+    """The zpool already exists"""
+    pass
+
+class ZfsPermissionError(ZfsOSError):
+    """The command could not be completed due to a permissions issue.
+
+    This is typically raised due to a lack of permissions for /dev/zfs (Linux) or
+    RBAC privileges (Solaris).
+    """
+    pass
+
+class ZfsArgumentError(ValueError):
+    """Zfs-specfic errors for parameters."""
+    pass
+
+class ZfsBadFsName(ZfsArgumentError):
+    """The filesystem or dataset name is malformed"""
+    pass
