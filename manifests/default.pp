a2913cb8cbfd08fe8bce15e1192463e8af2c0c76
Geoff Davis <gadavis@ucsd.edu>
2015-05-18 16:47:52 -0700

    Set up initial pools

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,6 +3,26 @@ Package {
 }
 if $::osfamily == 'RedHat' {
   class {'zfsonlinux': }
+
+}
+node 'target.test.int' {
+  exec { 'create poolfile':
+    command => '/bin/dd if=/dev/zero of=/zfsbackups-pool-file bs=1024 count=1024k',
+    creates => '/zfsbackups-pool-file',
+  } ->
+  zpool { 'zfsbackups':
+    ensure => present,
+    disk   => '/zfsbackups-pool-file',
+  }
 }
 
-
+node 'client.test.int' {
+  exec { 'create poolfile':
+    command => '/bin/dd if=/dev/zero of=/clienttest-pool-file bs=1024 count=1024k',
+    creates => '/clienttest-pool-file',
+  } ->
+  zpool { 'clienttest':
+    ensure => present,
+    disk   => '/clienttest-pool-file',
+  }
+}
