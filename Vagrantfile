d1975573bf0e345e2c8bfb86231219ab4dbe3f1d
Geoff Davis <gadavis@ucsd.edu>
2015-05-18 15:47:35 -0700

    Use zfsonlinux module to bootstrap ZoL
    Requires all kinds of submodules to do the tests

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,9 @@
 Vagrant.configure(2) do |config|
   config.vm.box = 'puppetlabs/centos-6.6-64-puppet'
 
-  config.vm.provision 'puppet'
+  config.vm.provision "puppet" do |puppet|
+    puppet.module_path = "modules"
+  end
 
   config.vm.define 'client' do |clienthost|
     clienthost.vm.hostname = 'client.test.int'
--- manifests/default.pp
+++ manifests/default.pp
@@ -2,19 +2,7 @@ Package {
   allow_virtual => true
 }
 if $::osfamily == 'RedHat' {
-  $epel_src = $::operatingsystemmajorrelease ? {
-    6       => 'http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm',
-    default => undef,
-  }
-  package { 'epel-release' :
-    source => $epel_src,
-  } ->
-  package { 'dkms' : } ->
-  package {'zfs-release':
-    source => 'http://archive.zfsonlinux.org/epel/zfs-release.el6.noarch.rpm',
-  } ->
-  package {'kernel-devel': } ->
-  package {'zfs': }
+  class {'zfsonlinux': }
 }
 
 
