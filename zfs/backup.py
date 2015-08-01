c8232656cf11bdda12b82bcf3189165a5bfb36d6
Geoff Davis <gadavis@ucsd.edu>
2015-07-31 18:16:59 -0700

    WIP: zfs.backup
    At the point where the command should successfully interrogate the
    target system for it's list of snapshots, but this hasn't been tested
    yet. Need to work on the vagrant VMs a bit to test.

--- zfs/backup.py
+++ zfs/backup.py
@@ -38,7 +38,7 @@ class MbufferedSSHBackup(Backup):
         print '\n'
         print 'self.userprop_name is '+self.userprop_name+'\n'
 
-    def take_backup(self, fsnames, snapchildren=False):
+    def take_backup(self, filesystems, snapchildren=False):
         """Back up a filesystem using the mbuffered SSH method
 
         This is a continual incremental setup. A full is only taken if the
@@ -49,7 +49,7 @@ class MbufferedSSHBackup(Backup):
         parameters.
         """
 
-        if isinstance(fsnames, basestring) and fsnames == '//':
+        if isinstance(filesystems, basestring) and filesystems == '//':
             single_list,recursive_list = get_userprop_datasets(
                 label = self.label, userprop_name=self.userprop_name)
 
@@ -64,19 +64,74 @@ class MbufferedSSHBackup(Backup):
 
             return
 
-        if isinstance(fsnames, basestring):
-            fsnames = [ fsnames ]
+        if isinstance(filesystems, basestring):
+            filesystems = [ filesystems ]
 
-        for fs in fsnames:
+        for fs in filesystems:
             logging.info("Looking for %s snapsnots of %s" % (
                 "recursive" if snap_children else "non-recursive",
                 fs))
 
+            # Get the current snapshots of the local fs
+            local_snaps=[ s[0] for s in zfs.util.zfs_list(fs,
+                                                          types=['snapshot'],
+                                                          depth=1,
+                                                          properties=['name'])
+                        ]
+
+            # Don't process this filesystem if it doesn't have any snapshots
+            if len(local_snaps) == 0:
+                logging.error('The filesystem %s fs does not have any snapshots.')
+                continue
+
             pool = get_pool_from_fsname(fs)
             guid = get_pool_guid(pool)
 
-            remote_backup_path="%s/%s/%s" % ( self.backup_zpool, guid, fs )
+            remote_base_path   = os.path.join( self.backup_zpool, guid)
+            remote_backup_path = os.path.join( remote_base_path, fs )
 
-            # check and create remote datasets
+            # check and create remote dataset
+            runner.zfs_create(remote_fs, create_parents=True)
 
+            # Get the current snapshots of the remote_fs
+            remote_snaps=[ s[0].lstrip(remote_base_path) for s in
+                          runner.zfs_list(remote_fs, types=['snapshot'],
+                                          depth=1) ]
 
+            want_remote_snapshot_purge = False
+            if len(remote_snaps) == 0:
+                backup_type = 'full'
+            elif len(local_snaps) == 1:
+                backup_type = 'full'
+            elif remote_snaps[-1] not in local_snaps:
+                backup_type = 'full'
+                want_remote_snapshot_purge = True
+            else:
+                backup_type = 'incremental'
+                # We only want the snapshot name itself
+                incremental_source = remote_snaps[-1].split('@', maxsplit=1)
+
+            newest_local_snap = local_snaps[-1]
+
+            # Now we're ready to send the backup to the remote system
+            send_backup(snapshot=newest_local_snap,
+                        incremental_source=incremental_source,
+                        remote_backup_path=remote_backup_path)
+
+    def send_backup(self, snapshot, remote_backup_path, incremental_source=None):
+        """Send a backup to the remote_backup_path on self.backup_host
+
+        :param incremental_source: optional name of the source snapshot to use for an incremental backup
+        :type incremental_source: str or None
+        :param snapshot: the source filesystem@snapshot
+        """
+        if incremental_source:
+            logging.info(
+                "Sending incremental backup %s %s to %s on remote host %s" % (
+                    incremental_source, snapshot, remote_backup_path,
+                    self.backup_host )
+            )
+        else:
+            logging.info("Sending full backup %s to %s on remote host %s" %
+                         (snapshot, remote_backup_path, self.backup_host))
+        pass
